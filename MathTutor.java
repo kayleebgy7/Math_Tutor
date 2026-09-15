e68cc09ea9d2be2c344f7a18862692f2bee7d215
kayleebgy7 <kgbegay999@gmail.com>
2026-09-15 18:20:30 +0000

    Added comments in code

--- MathTutor.java
+++ MathTutor.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
-pubilc class Math_Tutor {
+public class MathTutor
+{
+    import java.util.Scanner;
     public static void main(String[] args){
-        System.out.print("Enter 1 for Py")
+        System.out.print("Enter 1 for Pythagorean Theorem: ");
+        
     }
 }
