c95bda8f60d356f9e4c0ca0323ded348f5155417
Begay Kaylee <2193871@jeffcoschools.us>
2026-09-16 15:14:11 -0600

    Added code to the MathTutor Class

--- MathTutor.java
+++ MathTutor.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
+
 public class MathTutor {
 public static void main(String[] args){
+        System.out.println("Welcome to Math Tutor, please choose a question!");
+        System.out.println("Questions: Cylinder Volume(a), Pythagorean Theorm(b), Sphere Volume(c), and Sphere Surface Area(d)");
     Scanner input = new Scanner(System.in);
- 
+        String questionType = input.nextLine();
+
+        if (questionType.equalsIgnoreCase("a")){
+           
+        }
 
 
-
+input.close();
 }
 }
